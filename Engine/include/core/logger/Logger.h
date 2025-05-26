e10964bab8a0e9df192d6889236a4095dbf9c1c2
Tymur Slynko <timurslinko1@gmail.com>
2025-05-26 22:52:21 +0300

    docs: Documented Logger

--- Engine/include/core/logger/Logger.h
+++ Engine/include/core/logger/Logger.h
@@ -8,6 +8,10 @@ namespace DreamEngine::Core::Logger {
     class Logger {
     public:
         virtual ~Logger() = default;
+
+        /// @brief Logs a message with the specified log level.
+        /// @param level The log level of the message.
+        /// @paran msg The message to log.
         virtual void Log(LogLevel level, const std::string& msg) = 0;
     };
 
