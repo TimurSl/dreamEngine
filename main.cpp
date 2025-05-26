0b01b41395318b395dc5d8d4ce66d8f236f39f65
Tymur Slynko <timur6le@gmail.com>
2025-05-26 20:00:07 +0300

    chore: clang-format (#14)
    Co-authored-by: Tymur Slynko <timurslinko1@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
-int main() {
+int main()
+{
     return 0;
 }
 
 /// @brief Adds two integers.
 /// @return The sum of the two integers.
-int add(int a, int b) {
+int add(int a, int b)
+{
     return a + b;
 }
