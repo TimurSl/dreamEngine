56a1a6c9b0b89d57f31585947afaf7002fde526b
Tymur Slynko <timurslinko1@gmail.com>
2025-05-26 20:23:43 +0300

    feat: Setup editor_main.cpp entry point

---
+++ Editor/src/editor_main.cpp
@@ -0,0 +1,11 @@
+﻿//
+// Created by Tymur on 5/26/2025.
+//
+
+#include "editor_main.h"
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Editor started with " << argc << " arguments." << std::endl;
+}
