eba96f6eeca91d47c4698fd08c74728a496148b3
Tymur Slynko <timur6le@gmail.com>
2025-05-26 22:52:14 +0300

    Merge pull request #27
    * docs: Documented Logger

--- Engine/include/core/logger/LogLevel.h
+++ Engine/include/core/logger/LogLevel.h
@@ -2,6 +2,7 @@
 
 namespace DreamEngine::Core::Logger {
 
+    /// @brief Enumeration representing different log levels.
     enum class LogLevel {
         Trace,
         Debug,
--- Engine/include/core/logger/LogManager.h
+++ Engine/include/core/logger/LogManager.h
@@ -4,21 +4,32 @@
 #include <memory>
 #include <mutex>
 
+/// @brief Logging macros for trace level.
 #define DE_LOG_TRACE(msg)   ::DreamEngine::Core::Logger::LogManager::Instance().Log(::DreamEngine::Core::Logger::LogLevel::Trace,   msg)
+/// @brief Logging macros for debug level.
 #define DE_LOG_DEBUG(msg)   ::DreamEngine::Core::Logger::LogManager::Instance().Log(::DreamEngine::Core::Logger::LogLevel::Debug,   msg)
+/// @brief Logging macros for info level.
 #define DE_LOG_INFO(msg)    ::DreamEngine::Core::Logger::LogManager::Instance().Log(::DreamEngine::Core::Logger::LogLevel::Info,    msg)
+/// @brief Logging macros for warning level.
 #define DE_LOG_WARNING(msg) ::DreamEngine::Core::Logger::LogManager::Instance().Log(::DreamEngine::Core::Logger::LogLevel::Warning, msg)
+/// @brief Logging macros for error level.
 #define DE_LOG_ERROR(msg)   ::DreamEngine::Core::Logger::LogManager::Instance().Log(::DreamEngine::Core::Logger::LogLevel::Error,   msg)
+/// @brief Logging macros for fatal level.
 #define DE_LOG_FATAL(msg)   ::DreamEngine::Core::Logger::LogManager::Instance().Log(::DreamEngine::Core::Logger::LogLevel::Fatal,   msg)
 
 namespace DreamEngine::Core::Logger {enum class LogLevel;
 
     class LogManager {
     public:
+        /// @brief Returns the singleton instance of LogManager.
+        /// @return Reference to the LogManager instance.
         static LogManager& Instance();
 
+        /// @brief Sets the logger to be used by the LogManager.
         void SetLogger(std::shared_ptr<Logger> logger);
+        /// @brief Sets the current log level for the LogManager.
         void SetLevel(LogLevel level);
+        /// @brief Logs a message with the specified log level.
         void Log(LogLevel level, const std::string& msg);
 
     private:
