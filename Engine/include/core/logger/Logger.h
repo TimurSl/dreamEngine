c24307bb41d2b91ac0efcf0165ef9e086a66db17
Tymur Slynko <timurslinko1@gmail.com>
2025-05-27 15:27:46 +0300

    docs: Fixed logger docs little bit

--- Engine/include/core/logger/Logger.h
+++ Engine/include/core/logger/Logger.h
@@ -2,16 +2,15 @@
 
 #include <string>
 
-namespace DreamEngine::Core::Logger {
+namespace DreamEngine::Core::Logger
+{
     enum class LogLevel;
 
-    class Logger {
+    class Logger
+    {
     public:
         virtual ~Logger() = default;
 
-        /// @brief Logs a message with the specified log level.
-        /// @param level The log level of the message.
-        /// @paran msg The message to log.
         virtual void Log(LogLevel level, const std::string& msg) = 0;
     };
 
