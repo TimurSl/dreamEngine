bdfa88b766f93bf707ad1d95367e363b5713a86b
Tymur Slynko <timurslinko1@gmail.com>
2025-05-27 22:31:44 +0300

    feat: Add Allocator and ArenaAllocator implementations with MallocAllocator support

---
+++ Engine/include/core/alloc/ArenaAllocator.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <cstdint>
+
+#include "Allocator.h"
+
+// Linear arena allocator (без free для каждого блока, только Reset)
+class ArenaAllocator : public Allocator {
+public:
+    explicit ArenaAllocator(size_t totalSize);
+    ~ArenaAllocator();
+
+    void* Allocate(size_t size, size_t alignment = alignof(std::max_align_t)) override;
+    void Deallocate(void* ptr) override {} // ничего не делает
+    void Reset();
+
+private:
+    char* m_Buffer;
+    size_t m_Offset;
+    size_t m_TotalSize;
+};
---
+++ Engine/include/core/alloc/MallocAllocator.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "Allocator.h"
+
+
+class MallocAllocator : public Allocator {
+public:
+    void* Allocate(size_t size, size_t alignment) override;
+    void Deallocate(void* ptr) override;
+};
+
+
